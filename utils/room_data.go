d1836976ff25b95d6c940b445a163ff46ec3b202
alice <122629939+espcaa@users.noreply.github.com>
2025-10-18 22:39:11 +0200

    in airtable we trust

--- utils/room_data.go
+++ utils/room_data.go
@@ -15,7 +15,7 @@ func GetRoomData(bot structs.BotInterface, userRecordId string) (structs.Room, e
 	// Get the projects table
 	projectsTable := bot.GetAirtableClient().GetTable(dbID, "Projects")
 
-	formula := fmt.Sprintf(`{user}='%s'`, userRecordId)
+	formula := fmt.Sprintf(`{user} = RECORD_ID('%s')`, userRecordId)
 
 	projectRecords, err := projectsTable.GetRecords().
 		WithFilterFormula(formula).
