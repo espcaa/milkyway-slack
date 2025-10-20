c38a671fdec911454d568a00ff6049896eb1740d
alice <122629939+espcaa@users.noreply.github.com>
2025-10-21 01:45:53 +0200

    better sync

--- sync.sh
+++ sync.sh
@@ -14,6 +14,7 @@ git config core.sparseCheckout true
 # Sparse checkout setup
 echo "static/room/" > .git/info/sparse-checkout
 echo "static/projects/" >> .git/info/sparse-checkout
+echo "static/room_draft.png" >> .git/info/sparse-checkout
 
 # Fetch and checkout
 git fetch --depth=10 origin main
@@ -26,5 +27,7 @@ mkdir -p ressources/synced/room/
 mkdir -p ressources/synced/projects/
 mv tmp_milkyway/static/room/* ressources/synced/room/
 mv tmp_milkyway/static/projects/* ressources/synced/projects/
+mv tmp_milkyway/static/room_draft.png ressources/room.png
 mv ressources/synced/room/floor ressources/synced/
+
 rm -rf tmp_milkyway
